e9df1aa2736cad93a93397fab16f4ec01ae338fa
Nick Babcock <nbabcock19@hotmail.com>
2013-12-12 10:39:30 -0600

    Add incidents per hour

--- dps/query.py
+++ dps/query.py
@@ -12,3 +12,17 @@ def for_weekday_incidents(con):
     # representation of the date. This is needed because different libraries
     # have different meanings of the first day of the week.
     return { days[int(day)]: int(num) for day, num in rows }
+
+def for_hour_incidents(con):
+    """
+    Given a connection to the database, will return a list of the number of
+    incidents that occurred at a given time. The list is 24 in length with the
+    first element corresponding to the zeroth hour of the day (12am to 1am)
+    """
+    query = """ SELECT strftime('%H', Time), COUNT(*) FROM Crimes
+                GROUP BY strftime('%H', Time) """
+    data = [0] * 24
+    for row in con.execute(query):
+        hour, number = row[:]
+        data[int(hour)] = int(number)
+    return data
--- tests/test_query.py
+++ tests/test_query.py
@@ -5,11 +5,22 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from dps import query
 
 class MockWeek:
-    def execute(self, sting):
-        return {('2', '2000'), ('0', '1000'), ('5', '5000')}
+    def execute(self, _):
+        return [('2', '2000'), ('0', '1000'), ('5', '5000')]
+
+class MockHour:
+    def execute(self, _):
+        return [('01', '1570'), ('04', '554')]
 
 class TestQueryFunctions(unittest.TestCase):
     def test_weekdays(self):
         actual = query.for_weekday_incidents(MockWeek())
         expected = {'Sunday': 1000, 'Tuesday': 2000, 'Friday': 5000}
         self.assertDictEqual(expected, actual)
+
+    def test_hour(self):
+        actual = query.for_hour_incidents(MockHour())
+        expected = [0] * 24
+        expected[1] = 1570
+        expected[4] = 554
+        self.assertSequenceEqual(expected, actual)
