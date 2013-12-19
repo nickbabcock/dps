a1fe6e5bde4b69917d78989f7848b713c9b53a96
Nick Babcock <nbabcock19@hotmail.com>
2013-12-19 10:35:52 -0600

    Marker refactoring

--- static/js/dps.js
+++ static/js/dps.js
@@ -1,13 +1,16 @@
 (function() {
     'use strict';
 
-    var url = GMaps.staticMapURL({
-      size: [610, 300],
-      lat: 42.2756663,
-      lng: -83.7356758
-    });
+    var gmap = function(markers) {
+        var url = GMaps.staticMapURL({
+            size: [610, 300],
+            lat: 42.2756663,
+            lng: -83.7356758,
+            markers: markers
+        });
 
-    $('#map').attr('src', url);
+        $('#map').attr('src', url);
+    };
 
     var Person = function(data) {
         this.crime = data.crime;
@@ -26,16 +29,10 @@
             viewModel.incidents.push(new Person(results[i]));
             locations.push({ lat: results[i].latitude, lng: results[i].longitude });
         }
-
-        var url = GMaps.staticMapURL({
-          size: [610, 300],
-          lat: 42.2756663,
-          lng: -83.7356758,
-          markers: locations
-        });
-
-        $('#map').attr('src', url);
+        
+        gmap(locations);
     });
 
+    gmap();
     ko.applyBindings(viewModel);
 })();
