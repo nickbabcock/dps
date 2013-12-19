76dba819df8a303f0121346d567acff1e4708389
Nick Babcock <nbabcock19@hotmail.com>
2013-12-19 10:32:41 -0600

    Add markers

--- static/js/dps.js
+++ static/js/dps.js
@@ -21,9 +21,20 @@
 
     $.getJSON('/api/v1/latest', function(data) {
         var results = data.result;     
+        var locations = [];
         for (var i = 0; i < results.length; i++) {
             viewModel.incidents.push(new Person(results[i]));
+            locations.push({ lat: results[i].latitude, lng: results[i].longitude });
         }
+
+        var url = GMaps.staticMapURL({
+          size: [610, 300],
+          lat: 42.2756663,
+          lng: -83.7356758,
+          markers: locations
+        });
+
+        $('#map').attr('src', url);
     });
 
     ko.applyBindings(viewModel);
