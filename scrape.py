4d87749b5ac5eed4b1d5618212e95e518c332a06
Nick Babcock <nbabcock19@hotmail.com>
2013-12-24 14:04:18 -0600

    Document scrape functionality

--- scrape.py
+++ scrape.py
@@ -2,6 +2,10 @@ from datetime import date, datetime, timedelta
 from dps import parser
 import sys
 
+# When invoked without any arguments, this application will scrape yesterdays
+# data and add it to the database. Given a date, this application scape that
+# day and add that to the database.
+
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
         start = datetime.strptime(sys.argv[1], '%m-%d-%Y') 
